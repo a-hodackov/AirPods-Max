9e1ce569f68ef21c4d4bc7172b7443c3878089b2
a-hodackov <hodackoff1995@gmail.com>
2021-05-12 00:06:27 +0300

    add adaptive

---
+++ script.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  },
+};
+
+if (isMobile.any()) {
+  document.body.classList.add("_touch");
+} else {
+  document.body.classList.add("_pc");
+}
+
+const iconMenu = document.querySelector(".menu-icon");
+const menuBody = document.querySelector(".menu-body");
+const menuLinks = document.querySelectorAll(".menu-item");
+
+if (iconMenu) {
+  iconMenu.addEventListener("click", function () {
+    document.body.classList.toggle("_lock");
+    iconMenu.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
+  });
+}
+
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener("click", onMenuClick);
+});
+
+function onMenuClick() {
+  if (iconMenu.classList.contains("_active")) {
+    document.body.classList.remove("_lock");
+    iconMenu.classList.remove("_active");
+    menuBody.classList.remove("_active");
+  }
+}
